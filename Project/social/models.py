43cf0b81c4c496f3cb354dd2b800b43c8b988aaa
temalhaliti <halititemal@gmail.com>
2022-05-28 14:04:47 +0200

    Refactored code using a decorator

--- Project/social/models.py
+++ Project/social/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,6 +15,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
@@ -21,6 +23,6 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.follows.set([instance.profile.id])
         user_profile.save()
 
-post_save.connect(create_profile, sender=User)
+
 
 
