0668422052556a7ddd699ef893f8710272d7e6d5
temalhaliti <halititemal@gmail.com>
2022-05-28 14:30:05 +0200

    Added a profile list page

--- Project/social/urls.py
+++ Project/social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import base
+from .views import base, profilelist
 
 app_name = "social"
 
 urlpatterns = [
     path("", base, name="base"),
+    path("profilelist/", profilelist, name="profilelist"),
 ]
--- Project/social/views.py
+++ Project/social/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Profile
 
 def base(request):
     return render(request, "base.html")
+
+def profilelist(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    context = {"profiles": profiles}
+    return render(request, "profilelist.html", context)
