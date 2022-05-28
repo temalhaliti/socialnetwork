c9121b14f11acaacf5ca62dcc41642d3492e603c
temalhaliti <halititemal@gmail.com>
2022-05-28 14:57:52 +0200

    Added follow unfollow options

--- Project/social/views.py
+++ Project/social/views.py
@@ -11,6 +11,19 @@ def profilelist(request):
     return render(request, "profilelist.html", context)
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+        
     profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
     context = {"profile": profile}
     return render(request, "profile.html", context)
