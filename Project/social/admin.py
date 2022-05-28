55a825129ddd36df41842e292b7b2e83359dd44b
temalhaliti <halititemal@gmail.com>
2022-05-28 13:45:44 +0200

    Created a profie model, added to admin panel

--- Project/social/admin.py
+++ Project/social/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from .models import Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
@@ -8,5 +9,6 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.register(Profile)
 
 
