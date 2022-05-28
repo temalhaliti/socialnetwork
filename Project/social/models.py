d53dd779416188a86fa95ce0def3f05ee53fa1dc
temalhaliti <halititemal@gmail.com>
2022-05-28 14:01:23 +0200

    Added a post-save hook

--- Project/social/models.py
+++ Project/social/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,4 +10,17 @@ class Profile(models.Model):
         symmetrical=False,
         blank=True
     )
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
+
 
